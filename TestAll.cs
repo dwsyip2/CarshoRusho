2890b12b170d98e561ddcd62b921119b398b4ebf
User <4313526@students.swinburne.edu.my>
2017-05-28 03:29:44 +0800

    - test cases

--- TestAll.cs
+++ TestAll.cs
@@ -1,33 +1,30 @@
-using System;
 using SwinGameSDK;
 using NUnit.Framework;
-using System.Collections.Generic;
-using System.Diagnostics;
 namespace MyGame
 {
-	[TestFixture()]
+	[TestFixture ()]
 	public class TestAll
 	{
-		[Test()]
+		[Test ()]
 		public void TestDefaultInitialization ()
 		{
 			GameBoard myDrawing = new GameBoard ();
 			Assert.AreEqual (Color.DimGray, myDrawing.BackgroundColor);
 		}
 
-		[Test()]
-		public void TestInitializeWithColor()
+		[Test ()]
+		public void TestInitializeWithColor ()
 		{
-			GameBoard myDrawing = new GameBoard (Color.Blue); 
+			GameBoard myDrawing = new GameBoard (Color.Blue);
 			Assert.AreEqual (Color.Blue, myDrawing.BackgroundColor);
 		}
 
-		[Test()]
-		public void TestSpawnVehicle()
+		[Test ()]
+		public void TestSpawnVehicle ()
 		{
-			GameBoard gb = new GameBoard (); 
+			GameBoard gb = new GameBoard ();
 			PlayerVehicle p = new PlayerVehicle (415, 570);
-			ScoreBoard.Initialize(0, 3, 1, "Peak Hours");
+			ScoreBoard.Initialize (0, 3, 1, "Peak Hours");
 			Car c = new Car (415, 20);
 			Lorry l = new Lorry (415, 20);
 			Motorcycle m = new Motorcycle (415, 20);
@@ -42,8 +39,8 @@ namespace MyGame
 			Assert.AreEqual (UtilityFunction.InitialY, c.Y);
 		}
 
-		[Test()]
-		public void TestDrop()
+		[Test ()]
+		public void TestDrop ()
 		{
 			Car car = new Car (GameController.startLane1X, GameController.startLaneY);
 			PlayerVehicle p = new PlayerVehicle (GameController.startLane2X, 20);
@@ -51,13 +48,13 @@ namespace MyGame
 			car.SpeedY = 100;
 			SwinGame.Delay (10);
 			car.Drop (p);
-			Assert.AreNotEqual (GameController.startLaneY-1, car.Y);
+			Assert.AreNotEqual (GameController.startLaneY - 1, car.Y);
 		}
 
-		[Test()]
-		public void TestNagivate()
+		[Test ()]
+		public void TestNagivate ()
 		{
-			GameBoard gb = new GameBoard (); 
+			GameBoard gb = new GameBoard ();
 			PlayerVehicle p = new PlayerVehicle (415, 570);
 			ScoreBoard.Initialize (0, 0, 1, "Peak Hours");
 			Car c = new Car (415, 20);
@@ -66,17 +63,17 @@ namespace MyGame
 			Fuel f = new Fuel (415, 20);
 			Life lf = new Life (415, 20);
 			Invisible i = new Invisible (415, 20);
-            Score s = new Score (415, 20);
-            Bomb b = new Bomb (415, 20);
+			Score s = new Score (415, 20);
+			Bomb b = new Bomb (415, 20);
 			Turbo t = new Turbo (415, 20);
 
 			//right
 			p.SpeedX = 10000;
 			p.SpeedY = 10000;
 			SwinGame.Delay (20);
-			p.UpdateTime();
+			p.UpdateTime ();
 			p.NavigateRight ();
-			Assert.AreEqual (GameController.startLane3X+20, p.X);
+			Assert.AreEqual (GameController.startLane3X + 20, p.X);
 			////middle
 			//p.NavigateLeft ();
 			//Assert.AreEqual (GameController.startLane2X, p.X);
@@ -86,8 +83,8 @@ namespace MyGame
 
 		}
 
-		[Test()]
-		public void Testspeed()
+		[Test ()]
+		public void Testspeed ()
 		{
 			//Ensure that speed must be manually changed
 			Car c = new Car (415, 20);
@@ -98,31 +95,30 @@ namespace MyGame
 			SwinGame.Delay (1);
 			PlayerVehicle p = new PlayerVehicle (415, 570);
 			c.Drop (p);
-			Assert.IsTrue (c.SpeedY > 1);	
+			Assert.IsTrue (c.SpeedY > 1);
 		}
 
-		[Test()]
-		public void TestgameOver()
+		[Test ()]
+		public void TestgameOver ()
 		{
 			GameBoard gb = new GameBoard ();
 			PlayerVehicle p = new PlayerVehicle (415, 570);
-			ScoreBoard.Initialize(0, 0, 1, "Peak Hours");
+			ScoreBoard.Initialize (0, 0, 1, "Peak Hours");
 			Car c = new Car (415, 20);
 			Lorry l = new Lorry (415, 20);
 			Motorcycle m = new Motorcycle (415, 20);
 			Fuel f = new Fuel (415, 20);
 			Life lf = new Life (415, 20);
 			Invisible i = new Invisible (415, 20);
-            Score s = new Score (415, 20);
-            Bomb b = new Bomb (415, 20);
+			Score s = new Score (415, 20);
+			Bomb b = new Bomb (415, 20);
 			Turbo t = new Turbo (415, 20);
 
-			if (gb.GameOver() == true)
-			{
+			if (gb.GameOver () == true) {
 				ScoreBoard.Life = 3;
 				ScoreBoard.Score = 1;
 			}
-			Assert.AreEqual (3 ,ScoreBoard.Life);	
+			Assert.AreEqual (3, ScoreBoard.Life);
 		}
 
 		[Test ()]
